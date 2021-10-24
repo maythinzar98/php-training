5dd31c0996763b7ca4e5261f57dab6b6c3c0791c
maythinzar98 <maythinzar.jj@gmail.com>
2021-10-24 22:11:04 +0630

    [master] add tutorial_7

---
+++ tutorials/tutorial_7/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tutorial 7</title>
+  <link href="css/reset.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+  <div class="container">
+    <h1 class="title">Generate QR Code</h1>
+    <form action="generate.php" method="post">
+      <input type="text" name="text" placeholder="Enter Text ..." class="txtbox">
+      <input type="submit" name="btn" value="Generate" class="btn">
+    </form>
+  </div>
+</body>
+</html>
