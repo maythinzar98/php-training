d43e000a6268e1b73b0a0916314d61dfd4043f2b
May Thinzar <92421361+maythinzar98@users.noreply.github.com>
2021-10-28 12:38:09 +0630

    Update web.php

--- ReviewFixed/quickstart/routes/web.php
+++ ReviewFixed/quickstart/routes/web.php
@@ -1,8 +1,8 @@
 <?php
-use Illuminate\Support\Facades\Route;
-use App\Models\Task;
-use Illuminate\Http\Request;
-use App\Http\Controllers\TaskController;
+    use Illuminate\Support\Facades\Route;
+    use App\Models\Task;
+    use Illuminate\Http\Request;
+    use App\Http\Controllers\TaskController;
 
     Route::get("/",[TaskController::class,'displayTasks']);
     Route::post("/task",[TaskController::class,'addTasks']);
